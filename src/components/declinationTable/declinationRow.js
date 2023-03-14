20c3e699deef8dc5ce81e7d4538fbf6c18d1d71a
Patrick Ayoup <patrick.ayoup@gmail.com>
2023-03-14 00:06:48 -0400

    Fix checkmark

--- src/components/declinationTable/declinationRow.js
+++ src/components/declinationTable/declinationRow.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Checkmark from './checkmark';
+
 export default class DeclinationRow extends React.Component {
   constructor(props) {
     super(props);
@@ -16,10 +18,19 @@ export default class DeclinationRow extends React.Component {
 
   isCorrect(idx) {
     return (
-      this.props.showSolution &&
       this.state.answer[idx].toLowerCase() ===
       this.props.solution[idx].toLowerCase()
     );
+  }
+
+  resultBadge(idx) {
+    if (!this.props.showSolution) {
+      return null;
+    } else if (this.isCorrect(idx)) {
+      return <Checkmark />;
+    } else {
+      return <div>{this.props.solution[idx]}</div>;
+    }
   }
 
   render() {
@@ -34,11 +45,7 @@ export default class DeclinationRow extends React.Component {
               type="text"
             ></input>
           </div>
-          {this.isCorrect(0) ? (
-            <div><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check" viewBox="0 0 16 16">
-              <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
-            </svg></div>
-          ) : <div>{this.props.solution[0]}</div>}
+          {this.resultBadge(0)}
         </td>
 
         <td key={this.props.solution[1]}>
@@ -49,9 +56,7 @@ export default class DeclinationRow extends React.Component {
               type="text"
             ></input>
           </div>
-          {this.isCorrect(1) ? (
-            <span>{this.props.solution[1]}</span>
-          ) : null}
+          {this.resultBadge(1)}
         </td>
       </tr>
     );
